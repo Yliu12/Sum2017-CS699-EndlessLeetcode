b8193ed5d677c4754da5c059a21c436a758df443
yliu12 <yliu12@bsu.edu>
2017-05-26 16:17:41 +0800

    Reverse words in String III

---
+++ src/bsu/edu/cs699/ying/ReverseWordsinaStringIII.java
@@ -0,0 +1,26 @@
+package bsu.edu.cs699.ying;
+
+/**
+ * [Reverse Words in a String III](https://leetcode.com/problems/reverse-words-in-a-string-iii/#/description)
+ * Input: "Let's take LeetCode contest"
+ Output: "s'teL ekat edoCteeL tsetnoc"
+
+ * Created by yliu12 on 2017/5/26.
+ *
+ *30 / 30 test cases passed.
+ Status: Accepted
+ Runtime: 18 ms
+ Beats 53.19
+ */
+public class ReverseWordsinaStringIII {
+    public String reverseWords(String s) {
+        String[] wordArray= s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for(String word : wordArray){
+            sb.append(new StringBuilder(word).reverse()+" ");
+        }
+
+    return sb.toString().trim();
+    }
+
+}
